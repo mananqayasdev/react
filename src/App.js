484eb1fe93f4b8e9ae04fff128ca56dec22ded58
mananqayasdev <mrmuhammadqayas@gmail.com>
2024-01-24 10:32:50 -0500

    added 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       <>
         <div className="main-container">
           <Heading
-            title="How can we help?
+            title="How can we help? 1
 "
           />
 
